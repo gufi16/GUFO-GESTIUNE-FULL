88fe51d897dadcdfa6a45c742b6d09dc2964d836
gufi16 <ervinvarga@poshard.ro>
2026-02-18 14:11:58 +0200

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ro">
-      <body style={{ fontFamily: "Arial", padding: 20 }}>
-        {children}
-      </body>
+      <body style={{ margin: 0, fontFamily: "Arial, sans-serif" }}>{children}</body>
     </html>
-  );
+  )
 }
