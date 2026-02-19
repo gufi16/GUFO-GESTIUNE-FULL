83da7b685a530f6aad67e30531549bbc021f9441
gufi16 <ervinvarga@poshard.ro>
2026-02-19 14:24:05 +0200

    Delete gufo-ui-clean/app directory

--- gufo-ui-clean/app/(app)/products/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProductsPage() {
-  return (
-    <div>
-      <h1 style={{ marginTop: 0 }}>Produse</h1>
-      <p>Aici vom lega UI-ul la API ulterior.</p>
-    </div>
-  )
-}
