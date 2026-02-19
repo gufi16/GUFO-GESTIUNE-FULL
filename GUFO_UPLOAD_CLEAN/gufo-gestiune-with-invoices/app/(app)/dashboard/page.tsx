23c6b9979deb67bd43c8a946f08634012cbc06d7
gufi16 <ervinvarga@poshard.ro>
2026-02-19 14:38:05 +0200

    Delete GUFO_UPLOAD_CLEAN directory

--- GUFO_UPLOAD_CLEAN/gufo-gestiune-with-invoices/app/(app)/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <div>
-      <h1 style={{ marginTop: 0 }}>Dashboard</h1>
-      <p>Aici pui rezumat: total produse, stoc, intrări/ieșiri etc.</p>
-    </div>
-  )
-}
--- GUFO_UPLOAD_CLEAN/gufo-ui-clean/app/(app)/settings/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1 style={{ marginTop: 0 }}>Page</h1>
-      <p>Conținut UI placeholder.</p>
-    </div>
-  )
-}
