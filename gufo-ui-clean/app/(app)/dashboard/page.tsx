0493635d94dfebef66e1951b12094f8611a8fcf5
gufi16 <ervinvarga@poshard.ro>
2026-03-13 16:01:19 +0200

    Delete gufo-ui-clean/app directory

--- gufo-ui-clean/app/(app)/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <div>
-      <h1 style={{ marginTop: 0 }}>Dashboard</h1>
-      <p>Rezumat aplicație: total produse, stoc, intrări/ieșiri.</p>
-    </div>
-  )
-}
