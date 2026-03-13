e02ed58fd586e16e4eadcde6e10282be68ba77a9
gufi16 <ervinvarga@poshard.ro>
2026-03-13 16:01:12 +0200

    Delete lib directory

--- lib/useTenantId.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-
-export function useTenantId(): string {
-  const searchParams = useSearchParams();
-  return searchParams.get("tenantId") || "";
-}
